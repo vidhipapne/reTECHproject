cd9e3b79d72f6d494e7ab5b2cd0579e95f6458b0
vidhipapne <36477883+vidhipapne@users.noreply.github.com>
2019-01-17 16:31:13 +0530

    Add files via upload

---
+++ teacherlogin.php
@@ -0,0 +1,52 @@
+<?php 
+
+ $email=$_POST['email'];
+ $password=$_POST['Password'];
+
+ $conn= mysqli_connect('localhost','root','','college_db');
+ if(!$conn){
+ 	          die("connection failed:".mysqli_connect_error());
+            }
+              echo "connected successfully";
+ echo"<br>"; 
+ echo"<br>";
+
+
+$sql =("SELECT email, password FROM teacher_login WHERE email= '$email' AND password='$password'");
+$result=$conn->query($sql);
+
+if ($result->num_rows > 0){           
+
+    while($row = $result->fetch_assoc()) {
+      $pass=$row["password"] ;
+      if($pass==$password){
+       echo "Successfully logged in";
+       }
+ }
+    }
+    else {
+       	echo "login failed";
+         } 
+
+   echo("<br>");
+   echo("<br>");
+
+   
+?>
+<html>
+  <body>
+     <a href="question.html"> Assignment </a>
+   </body>
+   </html>
+
+
+
+
+
+
+
+
+
+
+
+
